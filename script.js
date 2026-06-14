9637bda63e4759040a1f4f6d912156772ab12d91
klar-sites <klar.sites@twixly.io>
2026-06-14 14:05:49 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 import { createKlarClient } from "https://editor.klar.website/sdk/content-static.js";
 
 /* ---- Klar client ----------------------------------------------------- */
-let projectId = 455;
+let projectId = 456;
 function getProjectId() {
   const data = localStorage.getItem("klar")
     ? JSON.parse(localStorage.getItem("klar"))
